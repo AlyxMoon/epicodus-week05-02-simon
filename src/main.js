54036d24cf12fa383473fbb2bd2fe06c550c0671
AlyxMoon <allisterkmoon@gmail.com>
2021-06-21 19:48:22 -0700

    fill out first function, generatePattern
    Co-Authored-By: Sho newman <sho.newman@gmail.com>
    Co-Authored-By: Kevin Funez <kevin.funez315@gmail.com>

--- src/main.js
+++ src/main.js
@@ -4,4 +4,21 @@ import 'bootstrap';
 import 'bootstrap/dist/css/bootstrap.min.css';
 import './css/styles.css';
 
-console.log('hello there');
+const pattern = [];
+
+function generatePattern () {
+  const randomNumber = Math.floor(Math.random() * 4);
+
+  const options = ['red', 'green', 'blue', 'yellow'];
+
+  pattern.push(options[randomNumber]);
+}
+
+generatePattern();
+console.log('pattern 1', pattern);
+generatePattern();
+console.log('pattern 2', pattern);
+generatePattern();
+console.log('pattern 3', pattern);
+generatePattern();
+console.log('pattern 4', pattern);
